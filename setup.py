476cc66928d6d9c0219ece4ae742aa1814809a5a
Eric Kudler <joeento@gmail.com>
2019-12-20 18:10:47 -0500

    Updates setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='app',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
         'pymongo',
-        'marshmallow'
+        'marshmallow',
+        'fpdf'
     ],
 )
