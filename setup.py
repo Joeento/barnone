e37b065ee9cae88fddb3cec5376ec58bc7768a7c
Eric Kudler <joeento@gmail.com>
2019-12-20 17:59:41 -0500

    Updates to setup to make it exportable into a dist file

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='app',
-    version='1.0.0',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
+        'pymongo',
+        'marshmallow'
     ],
 )
