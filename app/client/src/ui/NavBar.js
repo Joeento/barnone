83a86f1d49a2c1642b4bd2ebc5a01d3b8bc7fb91
Eric Kudler <joeento@gmail.com>
2020-03-08 15:02:22 -0400

    Some navbar styling

--- app/client/src/ui/NavBar.js
+++ app/client/src/ui/NavBar.js
@@ -7,30 +7,27 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
+import { faBarcode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import './NavBar.css';
 
 function NavBar(props) {
   return (
     <Navbar variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Barnone</Navbar.Brand>
+      <Navbar.Brand href="#home">
+        <FontAwesomeIcon icon={faBarcode} />
+        {' '}
+        Barnone
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
+
+      <Nav className="mr-auto">
+      </Nav>
+      <Form inline>
+        <Button variant="success">Login</Button>
+      </Form>
+
     </Navbar>
   );
 }
